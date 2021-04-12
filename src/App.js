4fcc78a084fe4a3eb60fed45193169f0c24b85e7
Mateus Nunes Campos <mncampos@inf.ufrgs.br>
2021-04-12 04:03:34 -0300

    Organization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './Components/HomePage.jsx';
+import './Components/HomePage.styles.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
