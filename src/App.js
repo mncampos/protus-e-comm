5d541bd8bafefae500703f87a06c0564f4c4e488
mncampos <mncampos@inf.ufrgs.br>
2021-04-15 01:07:37 -0300

    update menu-items with routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./Pages/HomePage/homepage.component.jsx";
-import "./Pages/HomePage/homepage.styles.scss";
+ 
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
